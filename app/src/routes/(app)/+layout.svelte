eadc30c86e7cba01abee0e913541b42d3e7058df
Guillaume_Bernier <guillaume.bernier@klarr.ca>
2024-04-01 11:13:58 -0400

    init commit

---
+++ app/src/routes/(app)/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import "$lib/app.pcss";
+    import { ModeWatcher } from "mode-watcher";
+  </script>
+   
+  <ModeWatcher />
+   
+  <slot />
---
+++ app/src/routes/(app)/auth/+layout@.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '$lib/app.pcss';
+	import { ModeWatcher } from 'mode-watcher';
+</script>
+
+<ModeWatcher />
+<div class="flex justify-center items-center h-screen">
+	<slot />
+</div>
