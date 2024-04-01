33f461abdddc78968be3b0471da16171c3b24852
Guillaume_Bernier <guillaume.bernier@klarr.ca>
2024-04-01 12:52:08 -0400

    type for pocketbase

--- app/src/routes/(app)/app.d.ts
+++ app/src/routes/(app)/app.d.ts
@@ -1,7 +1,13 @@
+import { TypedPocketBase, UserResponse } from "./pocketbase-types"
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
 	namespace App {
+		interface Locals {
+            pb: TypedPocketBase,
+			user: UserResponse
+        }
 		// interface Error {}
 		// interface Locals {}
 		// interface PageData {}
