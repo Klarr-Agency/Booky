e3de1f776ac8ac3249e032ec78373fa64b8fe899
Guillaume_Bernier <guillaume.bernier@klarr.ca>
2024-04-04 20:51:35 -0400

    grid added to transactions page

---
+++ app/src/routes/(app)/admin/transactions/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import DataTable from "$lib/components/ui/data-table/data-table.svelte";
+	import data from "./tasks.json";
+</script>
+
+
+<div class="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+	<DataTable {data} />
+</div>
