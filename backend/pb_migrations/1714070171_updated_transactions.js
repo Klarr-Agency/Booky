54957cf5294e7847b7c35df40833863c3ceb4f39
Guillaume_Bernier <guillaume.bernier@klarr.ca>
2024-04-25 20:30:35 -0400

    rules applied to transactions collection

---
+++ backend/pb_migrations/1714070171_updated_transactions.js
@@ -0,0 +1,24 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("rybgkg359i2537w")
+
+  collection.listRule = "@request.auth.id != \"\""
+  collection.viewRule = "@request.auth.id != \"\""
+  collection.createRule = "@request.auth.id != \"\""
+  collection.updateRule = "@request.auth.id != \"\""
+  collection.deleteRule = "@request.auth.id != \"\""
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("rybgkg359i2537w")
+
+  collection.listRule = ""
+  collection.viewRule = ""
+  collection.createRule = ""
+  collection.updateRule = ""
+  collection.deleteRule = ""
+
+  return dao.saveCollection(collection)
+})
---
+++ backend/pb_migrations/1714086608_updated_transactions.js
@@ -0,0 +1,18 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("rybgkg359i2537w")
+
+  collection.updateRule = "@request.auth.id != \"\" && userId = @request.auth.id"
+  collection.deleteRule = "@request.auth.id != \"\" && userId = @request.auth.id"
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("rybgkg359i2537w")
+
+  collection.updateRule = "@request.auth.id != \"\""
+  collection.deleteRule = "@request.auth.id != \"\""
+
+  return dao.saveCollection(collection)
+})
